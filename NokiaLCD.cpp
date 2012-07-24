193873a440a591b9d8b937fafa7448cbd89f9251
Nick <nhigh@me.com>
2012-07-23 23:39:51 -0400

    Added constructor implementation and sendSPI function.

--- NokiaLCD.cpp
+++ NokiaLCD.cpp
@@ -1,5 +1,31 @@
+#include "Arduino.h"
 #include "NokiaLCD.h"
 
 NokiaLCD::NokiaLCD(short CS, short SCK, short DIO, short RST) {
+    _CS = CS;
+    _SCK = SCK;
+    _DIO = DIO;
+    _RST = RST;
 
+    pinMode(_CS, OUTPUT);
+    pinMode(_SCK, OUTPUT);
+    pinMode(_DIO, OUTPUT);
+    pinMode(_RST, OUTPUT);
+}
+
+void NokiaLCD::sendSPI(unsigned char b, boolean isCommand) {
+    digitalWrite(_CS, LOW);
+    
+    //Send 9 bits starting with data bit, then moving to MSB;
+    digitalWrite(_SCK, LOW);
+    digitalWrite(_DIO, isCommand);
+    digitalWrite(_SCK, HIGH);
+
+    for (int i = 0; i < 8; i++) {
+        boolean nextBit = (b >> (7-i)) & 1;
+        digitalWrite(_SCK, LOW);
+        digitalWrite(_DIO, nextBit);
+        digitalWrite(_SCK, HIGH);
+    }
+    digitalWrite(_CS, HIGH);
 }
--- NokiaLCD.h
+++ NokiaLCD.h
@@ -1,4 +1,18 @@
+#ifndef NokiaLCD_h
+#define NokiaLCD_h
+
+#include "Arduino.h"
+
 class NokiaLCD {
     public:
         NokiaLCD(short CS, short SCK, short DIO, short RST);
-}
+        void sendSPI(unsigned char b, boolean isCommand);
+
+    private:
+        short _CS;
+        short _SCK;
+        short _DIO;
+        short _RST;
+};
+
+#endif
